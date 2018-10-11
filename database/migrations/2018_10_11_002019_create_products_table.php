1efa6d70772a01210711846e216fbc8c2757ab52
Reynaldo Esparza <reynaldoeg_esparza@hotmail.com>
2018-10-11 00:54:15 -0500

    Agregar campo store

--- database/migrations/2018_10_11_002019_create_products_table.php
+++ database/migrations/2018_10_11_002019_create_products_table.php
@@ -15,6 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('store');
             $table->string('name');
             $table->string('description');
             $table->string('price');
